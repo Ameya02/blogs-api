631c0dcf12d784320d0cc9b5e6778c039bd66d7b
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2024-03-08 22:20:08 +0530

    Added postgres

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
     const dbConfig = {
-      user: 'postgres',
-      password: 'root',
-      database: 'blogsDB',
-      host: '127.0.0.1',
-      port: 5432
+      user: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
     };
     const db = pgp(dbConfig);
 const callStoredProcedure = async (procedureName, ...args) => {
